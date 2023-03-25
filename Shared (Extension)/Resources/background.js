829c9b7d321aff6de0bb32b7da2e586666df56d2
Anmol Jain <anmolj678@gmail.com>
2023-03-25 01:14:12 -0700

    inital commit

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -1,6 +1,35 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
 
-    if (request.greeting === "hello")
-        sendResponse({ farewell: "goodbye" });
+let chatData = [];
+let currentTitleElementIndex = 0;
+
+browser.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  if (message.action === 'getCount') {
+      sendResponse(currentTitleElementIndex);
+  } else if (message.action === 'storeChatData') {
+      
+    const { id, messages, name } = message.chat;
+    try {
+      const mkMessages = messages.map(({ role, content: c }) => ({ role, content: convertHtmlToMarkdown(c) }) );
+      chatData.push({ id, messages: mkMessages, name });
+    } catch (err) {
+      console.log(err);
+    }
+    
+    currentTitleElementIndex++;
+    const tab = await browser.tabs.query({ active: true, currentWindow: true });
+    browser.tabs.sendMessage(tab[0].id, { action: 'scrape', count: currentTitleElementIndex });
+  } else if (message.action === 'processCompleted') {
+    const tab = await browser.tabs.query({ active: true, currentWindow: true });
+    browser.tabs.sendMessage(tab[0].id, { action: 'saveJSON', data: chatData.reverse() });
+    chatData = []; // Reset chat data after saving the file
+    currentTitleElementIndex = 0;
+  }
 });
+
+const turndownService = new TurndownService();
+
+function convertHtmlToMarkdown(html) {
+  return turndownService.turndown(`${html}`);
+}
+
+
--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,85 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
+
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'scrape') {
+    processTitleElement(message.count ?? 0)
+      .then(() => {
+        
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else if (message.action === 'saveJSON') {
+      downloadJSON(message.data);
+  }
 });
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+async function processTitleElement(index) {
+    
+    const saveChat = async () => {
+        return new Promise(async (resolve) => {
+            const id = window.location.href.match(/\/chat\/([\w-]+)$/)[1];
+
+            // Collect the chat messages
+            const messages = Array.from(
+                document.querySelectorAll(
+                  ".group div:first-child .flex-grow > div:first-child"
+                )
+            ).map((message, index) => ({
+                role: index % 2 === 0 ? "user" : "assistant",
+                content: message.innerHTML.trim()
+            }));
+
+            console.debug(
+                `Collected ${messages.length} messages from chat ${id}`
+            );
+
+            // Create the chat object
+            const chat = { id, messages };
+            resolve(chat);
+        });
+    }
+    
+  const titleElements = document.querySelectorAll('div.flex-1.text-ellipsis.max-h-5.overflow-hidden.break-all.relative');
+  
+    if (index >= titleElements.length || index > 4) {
+        const showMoreButton = document.querySelector("button.btn-dark");
+        if (showMoreButton) {
+            showMoreButton.click();
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            processTitleElement(index);
+            return;
+        }
+       
+        console.log('Finished processing title elements');
+        browser.runtime.sendMessage({ action: 'processCompleted' });
+        return;
+    }
+
+  const titleElement = titleElements[index];
+    
+    titleElement.addEventListener('click', async () => {
+      // Wait for the new page to load
+      setTimeout(async () => {
+        const chat = await saveChat();
+        chat.name = titleElement.textContent.trim();
+        console.debug(`Chat name: ${chat.name}`);
+        browser.runtime.sendMessage({ action: 'storeChatData', chat });
+      }, 3000);
+    });
+    
+    
+  titleElement.click();
+
+}
+
+function downloadJSON(data) {
+  const jsonString = JSON.stringify(data)
+  const blob = new Blob([jsonString], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'chat_data.json';
+  a.click();
+  URL.revokeObjectURL(url);
+}
+
